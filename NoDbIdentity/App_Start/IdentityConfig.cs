e89f603cc3e9d0f8ee464e13b648bbcd70bd3058
Flash Mission <mathlab4@hotmail.com>
2017-07-08 12:43:31 +0300

    f

---
+++ NoDbIdentity/App_Start/IdentityConfig.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using NoDbIdentity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace NoDbIdentity
+{
+    
+    public class InMemoryUserStore<T> : IUserStore<T> where T : ApplicationUser
+    {
+        private readonly static IList<T> _users = new List<T>();
+
+        public IQueryable<T> Users
+        {
+            get
+            {
+                return _users.AsQueryable();
+            }
+        }
+        private T FindUser(T user)
+        {
+            return _users.SingleOrDefault(x => x.Id == user.Id)?? _users.SingleOrDefault(x=>x.UserName == user.UserName);
+
+        }
+        private T FindUser(string userId)
+        {
+            return _users.SingleOrDefault(x => x.Id == userId);
+
+        }
+       
+        public Task DeleteAsync(T user)
+        {
+            lock (_users)
+            {
+                var existingUser = FindUser(user);
+                if (existingUser == null)
+                {
+                    return Task.FromResult(IdentityResult.Failed("User not found"));
+                }
+                _users.Remove(existingUser);
+            }
+            return Task.FromResult(IdentityResult.Success);
+        }
+
+        public Task<T> FindByIdAsync(string userId)
+        {
+            return Task.FromResult(FindUser(userId));
+        }
+
+        public Task<T> FindByNameAsync(string userName)
+        {
+            return Task.FromResult(_users.SingleOrDefault(x => x.UserName == userName));
+        }
+
+        public Task UpdateAsync(T user)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        void IDisposable.Dispose()
+        {
+            // throw new NotImplementedException();
+            
+        }
+
+        Task IUserStore<T, string>.CreateAsync(T user)
+        {
+            lock (_users)
+            {
+                var existingUser = FindUser(user);
+                if (existingUser == null)
+                {
+                    _users.Add(user);
+                    existingUser = user;
+
+                }
+                return Task.FromResult(existingUser);
+            }
+        }
+    }
+    public class InMemoryUserManager : UserManager<ApplicationUser>
+    {
+        public InMemoryUserManager(InMemoryUserStore<ApplicationUser> store)
+            : base(store)
+        {
+
+        }
+        public static InMemoryUserManager Create(IdentityFactoryOptions<InMemoryUserManager> options, IOwinContext context)
+        {
+
+            return new InMemoryUserManager(new InMemoryUserStore<ApplicationUser>());
+        }
+        //
+        // Summary:
+        //     Returns the user associated with this login
+        //public virtual Task<TUser> FindAsync(UserLoginInfo login);
+        public Task<ApplicationUser> FindAsync(ExternalLoginInfo ext)
+        {
+            return Store.FindByIdAsync(ext.Login.ProviderKey);
+
+        }
+        public Task<ApplicationUser> FindAByNameAsync(string username)
+        {
+            return Store.FindByNameAsync(username);
+
+        }
+        public Task<ApplicationUser> CreateAsync(ExternalLoginInfo ext)
+        {
+            var newUser = new ApplicationUser { Id = ext.Login.ProviderKey, UserName = ext.DefaultUserName };
+
+            return (Task<ApplicationUser>)Store.CreateAsync(newUser);
+        }
+
+        public Task<ApplicationUser> CreateAsync(string userName)
+        {
+            var newUser = new ApplicationUser { Id = Guid.NewGuid().ToString(), UserName = userName };
+
+            return (Task<ApplicationUser>)Store.CreateAsync(newUser);
+        }
+       
+
+    }
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+    {
+        public ApplicationSignInManager(InMemoryUserManager userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((InMemoryUserManager)UserManager);
+        }
+
+        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
+        {
+            return new ApplicationSignInManager(context.GetUserManager<InMemoryUserManager>(), context.Authentication);
+        }
+
+        public async Task<SignInStatus> SignInMemoryUserAsync(ApplicationUser user, bool isPersistent,bool remenberMe)
+        {
+
+            await SignInAsync(user, isPersistent, remenberMe);
+            return SignInStatus.Success;
+        }
+    }
+
+}
---
+++ NoDbIdentity/Controllers/AccountController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using NoDbIdentity.Models;
+using System.Security.Principal;
+
+namespace NoDbIdentity.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private ApplicationSignInManager _signInManager;
+       
+
+        public AccountController()
+        {
+        }
+
+        public AccountController(InMemoryUserManager userManager, ApplicationSignInManager signInManager )
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set 
+            { 
+                _signInManager = value; 
+            }
+        }
+
+        private InMemoryUserManager _userManager;
+        public InMemoryUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<InMemoryUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        //
+        // GET: /Account/Login
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            IPrincipal iUser = HttpContext.User;
+            if (!iUser.Identity.IsAuthenticated)
+            {
+                var user = await UserManager.FindByNameAsync(model.Email) ?? await UserManager.CreateAsync(model.Email);
+                var result = await SignInManager.SignInMemoryUserAsync(user,false, model.RememberMe);
+
+                switch (result)
+                {
+                    case SignInStatus.Success:
+                        return RedirectToLocal(returnUrl);
+
+                    case SignInStatus.Failure:
+                    default:
+                        ModelState.AddModelError("", "Invalid login attempt.");
+                        return View(model);
+                }
+            }
+            return View(model);
+           
+        }
+
+     
+      
+       
+        // POST: /Account/LogOff
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> LogOff()
+        {            
+            await UserManager.DeleteAsync(new ApplicationUser { Id = User.Identity.GetUserId(), UserName = User.Identity.GetUserName() });
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("Index", "Home");
+        }
+
+       
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_userManager != null)
+                {
+                    _userManager.Dispose();
+                    _userManager = null;
+                }
+
+                if (_signInManager != null)
+                {
+                    _signInManager.Dispose();
+                    _signInManager = null;
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Helpers      
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+       
+        #endregion
+    }
+}
